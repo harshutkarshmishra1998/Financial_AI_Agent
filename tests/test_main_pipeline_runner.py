4f0c4f5ea2f45b7af4b8eb9e8f1fcb6b8e9b0b0a
Utkarsh Mishra <harshutkarshmishra1998@gmail.com>
2026-02-18 23:02:45 +0530

    Testing Done - Looks OK

--- tests/test_main_pipeline_runner.py
+++ tests/test_main_pipeline_runner.py
@@ -1,30 +1,10 @@
-# from data_acquisition.pipeline.main_acquisition_runner import process_last_n_queries
+from data_acquisition.pipeline.main_acquisition_runner import process_last_n_queries
 
 
-# print("\n===== MAIN ACQUISITION RUNNER TEST =====")
+print("\n===== MAIN ACQUISITION RUNNER TEST =====")
 
-# results = process_last_n_queries(3)
+results = process_last_n_queries(50)
 
-# print("\nRESULT SUMMARY")
-# for r in results:
-#     print(r)
-
-from data_acquisition.pipeline.main_acquisition_runner import _load_query_logs
-from data_acquisition.pipeline.acquisition_planner import AcquisitionPlanner
-from data_acquisition.pipeline.acquisition_pipeline import MultiDomainAcquisitionPipeline
-
-queries = _load_query_logs()[-3:]
-
-planner = AcquisitionPlanner()
-pipeline = MultiDomainAcquisitionPipeline()
-
-for q in queries:
-    print("\nQUERY:", q.get("query_id"))
-
-    plan = planner.build_plan(q)
-    result = pipeline.run(plan, query=q)
-
-    print("domains returned:", list(result.domain_results.keys()))
-
-    market_data = result.domain_results.get("market", {})
-    print("market assets:", list(market_data.keys()))
+print("\nRESULT SUMMARY")
+for r in results:
+    print(r)
