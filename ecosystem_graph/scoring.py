17a42873a852f4e8d8efcb6939a0f9b4f8e30388
Utkarsh Mishra <harshutkarshmishra1998@gmail.com>
2026-03-01 21:25:48 +0530

    Restarting Phase 2

--- ecosystem_graph/scoring.py
+++
@@ -1,9 +0,0 @@
-def compute_node_importance(stat_strength, proximity, economic_weight):
-    return (
-        0.4 * stat_strength +
-        0.3 * proximity +
-        0.3 * economic_weight
-    )
-
-def edge_confidence(corr, granger_p):
-    return 0.6 * abs(corr) + 0.4 * (1 - granger_p)
